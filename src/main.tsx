33c04f3e9c08f1794a0f5be22451ecf7f088dc2a
hyungjinkim500 <hyungjinkim500@gmail.com>
2026-04-23 05:48:47 +0000

    fix: register service worker in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
