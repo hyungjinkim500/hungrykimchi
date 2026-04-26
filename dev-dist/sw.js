8e50b30474b71c5067ce4b1fafde81c95be517fb
hyungjinkim500 <hyungjinkim500@gmail.com>
2026-04-26 01:39:45 +0000

    feat: og 메타태그 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-de42eff6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l2urle6pfbg"
+    "revision": "0.0aj5fsphd1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
